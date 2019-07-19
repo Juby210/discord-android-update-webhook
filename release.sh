03a179214eeafcddd0fe8d535e7931a867227899
juby210-PL <juby210@vp.pl>
2019-07-20 00:13:47 +0200

    update release script

--- release.sh
+++ release.sh
@@ -12,9 +12,9 @@ zip -r release/DAUW_linux_amd64.zip DAUW config.example.json
 rm DAUW
 echo "Creating DAUW_windows_amd64.zip"
 cp build/DAUW.exe DAUW.exe
-zip -r release/DAUW_windows_amd64.zip build/DAUW.exe config.example.json
+zip -r release/DAUW_windows_amd64.zip DAUW.exe config.example.json
 rm DAUW.exe
 echo "Creating DAUW_freebsd_amd64.zip"
 cp build/DAUW_freebsd DAUW_freebsd
-zip -r release/DAUW_freebsd_amd64.zip build/DAUW_freebsd config.example.json
+zip -r release/DAUW_freebsd_amd64.zip DAUW_freebsd config.example.json
 rm DAUW_freebsd
